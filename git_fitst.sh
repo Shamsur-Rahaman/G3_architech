028c689671754bc1552460563cd324d9f49bb553
Shamsur-Rahaman <shamsurrahaman132@gmail.com>
2023-09-01 22:20:57 +0600

    responsiveness for banner added

--- git_fitst.sh
+++ git_fitst.sh
@@ -1,7 +1,7 @@
 
 git init
 git add .
-git commit -m "banner added"
+git commit -m "responsiveness for banner added"
 git branch -M main
 git remote add origin https://github.com/Shamsur-Rahaman/G3_architech.git
 git push -u origin main
---
+++ git_regular.sh
@@ -0,0 +1,6 @@
+
+
+
+git add .
+git commit -m "responsiveness for banner added"
+git push
